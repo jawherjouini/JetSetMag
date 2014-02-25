67c24cb86a693300ce31f2789d2bc9444da34374
Jawher Jouini <jawher.jouini@esprit.tn>
2014-02-25 19:54:13 +0100

    class de test
    Signed-off-by: Jawher Jouini <jawher.jouini@esprit.tn>

--- src/tn/esprit/projet/view/client/TEST.java
+++ src/tn/esprit/projet/view/client/TEST.java
@@ -6,10 +6,28 @@
 
 package tn.esprit.projet.view.client;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Jawher
  */
-public class TEST {
+public class TEST extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
     
 }
