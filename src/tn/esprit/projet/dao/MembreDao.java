d821e276cf91f96366c6fd08b4367c1548121410
Jawher Jouini <jawher.jouini@esprit.tn>
2014-02-26 20:50:16 +0100

    ahhhhhhhhhhhhhhhhhhhhh
    Signed-off-by: Jawher Jouini <jawher.jouini@esprit.tn>

--- src/tn/esprit/projet/dao/MembreDao.java
+++ src/tn/esprit/projet/dao/MembreDao.java
@@ -3,13 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tn.esprit.projet.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import tn.esprit.projet.model.Article;
+import tn.esprit.projet.util.Connexion;
 
 /**
  *
  * @author acer
  */
 public class MembreDao {
-    
+
+    public boolean authentification(String username, String pass) {
+        Statement stmt;
+        boolean result = false;
+        Connection cnx;
+        ResultSet rs;
+
+        try {
+            cnx = Connexion.getInstance();
+            String query = "Select * from membre where username='"+username+"' and password='"+pass+"'";
+            stmt = cnx.createStatement();
+            rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                result = true;
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Listage impossible: " + ex.getMessage());
+        }
+        return result;
+    }
 }
+
