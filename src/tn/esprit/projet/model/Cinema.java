0a53e7f17744a9e5218af12503355998d5f4bcc5
helakhechine <hela.khechine@esprit.tn>
2014-02-26 20:16:51 +0100

    git

---
+++ src/tn/esprit/projet/model/Cinema.java
@@ -0,0 +1,78 @@
+package tn.esprit.entities;
+
+public class Cinema {
+	
+	
+	int id_Salle;
+	String Adresse;
+	
+	
+	
+	public Cinema() {
+		super();
+	}
+
+	public Cinema(int id_Salle) {
+		super();
+		this.id_Salle = id_Salle;
+	}
+
+	public Cinema(int id_Salle, String adresse) {
+		super();
+		this.id_Salle = id_Salle;
+		Adresse = adresse;
+	}
+	
+	public int getId_Salle() {
+		return id_Salle;
+	}
+	public void setId_Salle(int id_Salle) {
+		this.id_Salle = id_Salle;
+	}
+	public String getAdresse() {
+		return Adresse;
+	}
+	public void setAdresse(String adresse) {
+		Adresse = adresse;
+	}
+
+	@Override
+	public String toString() {
+		return "Cinema [id_Salle=" + id_Salle + ", Adresse=" + Adresse + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Adresse == null) ? 0 : Adresse.hashCode());
+		result = prime * result + id_Salle;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cinema other = (Cinema) obj;
+		if (Adresse == null) {
+			if (other.Adresse != null)
+				return false;
+		} else if (!Adresse.equals(other.Adresse))
+			return false;
+		if (id_Salle != other.id_Salle)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+
+}
