ae265073e5a155bf7fd85314b1c8c4705d578cea
Jawher Jouini <jawher.jouini@esprit.tn>
2014-02-26 21:31:23 +0100

    ahhhhhhhhhhhhhhhhhhhhh
    Signed-off-by: Jawher Jouini <jawher.jouini@esprit.tn>

--- src/tn/esprit/projet/dao/MembreDao.java
+++ src/tn/esprit/projet/dao/MembreDao.java
@@ -18,24 +18,25 @@ import tn.esprit.projet.util.Connexion;
  */
 public class MembreDao {
 
-    public boolean authentification(String username, String pass) {
+    public int authentification(String username, String pass) {
         Statement stmt;
-        boolean result = false;
+        int result = 0;
         Connection cnx;
         ResultSet rs;
 
         try {
             cnx = Connexion.getInstance();
-            String query = "Select * from membre where username='"+username+"' and password='"+pass+"'";
+            String query = "Select * from membre where username='" + username + "' and password='" + pass + "'";
             stmt = cnx.createStatement();
             rs = stmt.executeQuery(query);
             if (rs.next()) {
-                result = true;
+                result = rs.getInt("isAdministrateur") + 1;
             }
+            System.out.println(result);
+
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Listage impossible: " + ex.getMessage());
         }
         return result;
     }
 }
-
