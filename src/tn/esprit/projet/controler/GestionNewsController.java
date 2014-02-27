0643e68c20b0bd0179be632d660e9714b3c880eb
Jawher Jouini <jawher.jouini@esprit.tn>
2014-02-27 11:55:56 +0100

    ahhhhhhhhhhhhhhhhhhhhh
    Signed-off-by: Jawher Jouini <jawher.jouini@esprit.tn>

--- src/tn/esprit/projet/controler/GestionNewsController.java
+++ src/tn/esprit/projet/controler/GestionNewsController.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tn.esprit.projet.controler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,9 +20,12 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.HTMLEditor;
 import tn.esprit.projet.dao.ArticleDAO;
 import tn.esprit.projet.dao.CommonDAO;
+import tn.esprit.projet.model.Article;
 import tn.esprit.projet.util.ControlledScreen;
 import tn.esprit.projet.util.ScreensController;
 
@@ -30,79 +34,67 @@ import tn.esprit.projet.util.ScreensController;
  *
  * @author oukhay
  */
-public class GestionNewsController implements Initializable,ControlledScreen {
+public class GestionNewsController implements Initializable, ControlledScreen {
+
     ScreensController screencontroller;
-    CommonDAO cdao = new CommonDAO();
-    @FXML
-    private MenuBar menu;
-    @FXML
-    private Menu fichier;
-    @FXML
-    private MenuItem profil;
-    @FXML
-    private Menu films;
-    @FXML
-    private Menu news;
-    @FXML
-    private Menu events;
-    @FXML
-    private Menu membres;
-    @FXML
-    private Menu salles;
-    @FXML
-    private Menu comms;
-    @FXML
-    private Menu stats;
-    @FXML
-    private TabPane tabpane;
-    @FXML
-    private Tab onglet_cons;
     @FXML
     private Button btn_del;
     @FXML
-    private TableView<Object> tab;
+    private TableView<Article> tab;
+    @FXML
+    private Button btn_deltt;
+    CommonDAO cdao = new CommonDAO();
+    ArticleDAO adao = new ArticleDAO();
+    @FXML
+    private Tab onglet_consult;
+    @FXML
+    private VBox vb;
     @FXML
     private Tab onglet_ajout;
     @FXML
-    private TextField tf_titre_article_a;
-    @FXML
-    private Button btn_ajout;
-    @FXML
-    private HTMLEditor he_texte_a;
-    @FXML
     private Tab onglet_modif;
-    @FXML
-    private HTMLEditor he_texte_m;
-    @FXML
-    private TextField tf_id_article;
-    @FXML
-    private Button btn_modifier;
-    @FXML
-    private TextField tf_titre_article_m;
-    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-      charger();
-      
-    }  
-    
+        charger();
+    }
 
-      
     public void charger() {
         tab.getColumns().clear();
         for (String s : cdao.ColonneName("article")) {
             TableColumn t = new TableColumn(s);
+            t.setCellValueFactory(new PropertyValueFactory(s));
+            t.setMinWidth(100);
+            t.setMaxWidth(200);
             tab.getColumns().add(t);
         }
+//        ObservableList<Object[]> items = cdao.finAll("article");
+//        System.out.println(items.toString());
+//        tab.getItems().setAll(items);
+        ObservableList<Article> items = adao.ListerArticles();
+        tab.setItems(items);
     }
 
     @Override
     public void SetScreenParent(ScreensController screenpage) {
         screencontroller = screenpage;
     }
-    
+
+    @FXML
+    private void supprimerLigne(ActionEvent event) {
+        TableView.TableViewFocusModel model = tab.getFocusModel();
+        System.out.println(model.getFocusedItem());
+        adao.deleteByElement((Article) model.getFocusedItem());
+        charger();
+    }
+
+    @FXML
+    private void supprimerTout(ActionEvent event) {
+        adao.delete();
+        charger();
+    }
+
 }
